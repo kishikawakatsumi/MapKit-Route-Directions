4fb556fbfcf4f885cb9674855ee02ca8c01f2626
kishikawakatsumi <kishikawakatsumi@mac.com>
2009-12-19 20:41:34 +0900

    stringWithContentsOfFile is deprecated. use stringWithContentsOfURL:encoding:error: instead.

--- Classes/UICGoogleMapsAPI.m
+++ Classes/UICGoogleMapsAPI.m
@@ -33,12 +33,12 @@
 
 - (void)makeAvailable {
 	NSString *filePath = [[NSBundle mainBundle] pathForResource:@"api" ofType:@"html"];
-	NSString *fileContents = [NSString stringWithContentsOfFile:filePath];
+	NSString *fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
 	[self loadHTMLString:fileContents baseURL:[NSURL URLWithString:@"http://iphone.local/"]];
 }
 
 - (void)webView:(UIWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(id)frame {
-//	NSLog(@"%@", message);
+	NSLog(@"%@", message);
 	id JSONValue = [message JSONValue];
 	if (!JSONValue) {
 		if ([self.delegate respondsToSelector:@selector(goolgeMapsAPI:didFailWithMessage:)]) {
