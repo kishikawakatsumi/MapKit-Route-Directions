1d89c71906e5ecd3e629bae288469c98b9adb9e5
kishikawakatsumi <kishikawakatsumi@mac.com>
2010-02-12 21:47:58 +0900

    revert google api access before.

--- Classes/UICGPolyline.m
+++ Classes/UICGPolyline.m
@@ -25,7 +25,7 @@
 	if (self != nil) {
 		dictionaryRepresentation = [dictionary retain];
 		
-        vertices = [[dictionaryRepresentation objectForKey:[[dictionaryRepresentation allKeys] objectAtIndex:4]] retain];
+        vertices = [[dictionaryRepresentation objectForKey:@"k"] retain];
 		vertexCount = [vertices count];
 		routePoints = [NSMutableArray arrayWithCapacity:vertexCount];
 		for (NSDictionary *vertex in vertices) {
